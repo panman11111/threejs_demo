c4edecd468a2de6b1ca3e49502830fcb8cebe4ef
Takuto Sakuma <pan.work11111@gmail.com>
2023-06-04 22:33:19 +0900

    three.js demo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Three_js />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Three_js from "./components/Three_js.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Three_js,
+  },
+});
+</script>
